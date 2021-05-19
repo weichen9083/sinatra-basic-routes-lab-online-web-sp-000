24f48d39c88b11e80a8afef0be398b591ca97159
wei chen <weichen9083@gmail.com>
2021-05-19 23:29:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+      get '/' do
+      "Hello, World!"
+    end
+  
+  
+  get '/name' do 
+    "My name is wei"
+  end 
+  get '/hometown' do 
+    "My hometown is ling"
+  end 
+  get "/favorite-song" do 
+    "My favorite song is fight song "
+  end 
 end
